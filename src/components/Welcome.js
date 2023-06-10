535ccb160b3edd0ab71297bb1e548ddc8543c15a
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-10 14:06:14 +0530

    submitting code-Sat Jun 10 2023 14:06:12 GMT+0530 (India Standard Time)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+
+
+const Welcome = ({name}) =>{
+    return(
+        <div>
+               <h1>Hey {name}!</h1>
+                <h2>Welcome to School.</h2>
+        </div>   
+
+    )
+}
+
+export default Welcome;
