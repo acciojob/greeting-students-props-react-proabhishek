9a4532d5de4c810a90a6fe17f972cf0e46adcca1
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-10 14:12:14 +0530

    submitting code-Sat Jun 10 2023 14:12:11 GMT+0530 (India Standard Time)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,10 @@
 // write code for Welcome component here
-
+import React from "react";
 
 const Welcome = ({name}) =>{
     return(
         <div>
-               <h1>Hey {name}!</h1>
+                <h1>Hey {name}!</h1>
                 <h2>Welcome to School.</h2>
         </div>   
 
